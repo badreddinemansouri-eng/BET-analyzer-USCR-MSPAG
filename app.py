972d537292a201eeea2d4a6ce92816ffb720f782
badreddinemansouri-eng <badreddine.mansouri@etudiant-fst.utm.tn>
2026-01-20 14:50:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2194,7 +2194,7 @@ class IUPACBETAnalyzer:
                 'porosity_type': 'Unknown'
             }
 
-       assert results['final_total_volume'] > 0, "Pore volume calculation failed"
+            assert results['final_total_volume'] > 0, "Pore volume calculation failed"
 
 
     def _estimate_pore_properties(self):
@@ -4276,6 +4276,7 @@ def display_ultra_hd_analysis_results(analyzer):
 
 if __name__ == "__main__":
     main()
+
 
 
 
